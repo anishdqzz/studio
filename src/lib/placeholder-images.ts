9d71c319f7cd65f743443d3d051318bb64d2fe49
ANISH <mpanish032@gmail.com>
2026-02-08 13:32:46 +0000

    TEXT

--- src/lib/placeholder-images.ts
+++ src/lib/placeholder-images.ts
@@ -9,6 +9,12 @@ import image6 from './IMG_5104 (1).jpg.jpeg';
 import image7 from './InShot_20251225_120528513.jpg.jpeg';
 import image8 from './Picsart_25-11-13_08-00-43-103.jpg.jpeg';
 import image9 from './Picsart_26-01-04_17-12-34-125.jpg.jpeg';
+import image10 from './Picsart_26-01-06_08-15-42-977.jpg.jpeg';
+import image11 from './IMG-20260127-WA0010.jpg.jpeg';
+import image12 from './IMG_20251112_130039 (2).png';
+import image13 from './IMG_20240903_164945_031.webp';
+import Localvideosrc from './InShot_20260116_204119246.mp4';
+import Localvideosrc from './src/lib/VID-20260105-WA0005.mp4';
 
 export type ImagePlaceholder = {
   id: string;
@@ -27,6 +33,10 @@ const imageMap: Record<string, any> = {
   gallery7: image7,
   gallery8: image8,
   gallery9: image9,
+  gallery10: image10,
+  gallery11: image11,
+  gallery12: image12,
+  gallery13: image13,
 };
 
 export const PlaceHolderImages: ImagePlaceholder[] = data.placeholderImages.map(img => ({
